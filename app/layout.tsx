f2608eb5097d7c84cca12bae74081d32de97a282
Shreyas Goyal <xenshreyas@gmail.com>
2026-04-13 18:05:47 -0700

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,13 @@ export const metadata: Metadata = {
     template: 'shreyas | %s',
   },
   description: siteConfig.description,
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
   alternates: {
     canonical: siteUrl,
   },
