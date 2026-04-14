d31e9bd77cb85f1d3d033d1b60ba210ddbe215b8
Shreyas Goyal <xenshreyas@gmail.com>
2026-04-13 18:11:42 -0700

    Fix build errors

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,36 +1,12 @@
 import { siteConfig, siteUrl } from 'app/data/site'
-import { getBlogPosts } from 'app/blog/utils'
 
 export async function GET() {
-  const allBlogs = await getBlogPosts()
-
-  const itemsXml = allBlogs
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1
-      }
-      return 1
-    })
-    .map(
-      (post) =>
-        `<item>
-          <title>${post.metadata.title}</title>
-          <link>${siteUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
-          <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
-        </item>`
-    )
-    .join('\n')
-
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
     <channel>
         <title>${siteConfig.defaultTitle}</title>
         <link>${siteUrl}</link>
         <description>${siteConfig.description}</description>
-        ${itemsXml}
     </channel>
   </rss>`
 
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,12 @@
-import { getBlogPosts } from 'app/featured/utils'
 import { siteUrl } from 'app/data/site'
 
 export const baseUrl = siteUrl
 
 export default async function sitemap() {
-  const blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
-
-  const routes = ['', '/work', '/projects', '/blog'].map((route) => ({
+  const routes = ['', '/work', '/projects', '/featured'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return routes
 }
